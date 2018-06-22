4c20db59cab003637198f62c623bbcef694b3566
Jan Van Zeghbroeck <janvanzeghbroeck@gmail.com>
2018-06-22 09:00:54 -0700

    card tests

--- cardbot/card.py
+++ cardbot/card.py
@@ -1,18 +1,42 @@
-from enum import Enum
+from enum import IntEnum #sees them as ints instead of just unique values
+import attr
 
 
-class Card(object):
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
+# two spaces
+@attr.s
+class Card: #for python 3 (object) is redundant
+    # def __init__(self, rank, suit):
+    #     self.rank = rank
+    #     self.suit = suit
+    suit = attr.ib()
+    rank = attr.ib()
+
+    @suit.validator
+    def is_suit(self, attribute, value):
+        if not isinstance(value, Suit): raise ValueError()#is value a part of class Suit
+
+    @rank.validator
+    def is_rank(self, attribute, value):
+        if not isinstance(value, Rank): raise ValueError()#is value a part of class Suit
+    # one space here
+
 
     # one space here
-    def __repr__(self):
+    def __str__(self): # i am going to display this to user, repr = display to coder
         return 'the {} of {}s'.format(self.rank.name.title(), self.suit.name.title())
 
+    # this exists
+    # __cmp__(self):
+    # __eq__(self):
 
 # two spaces here
-class Rank(Enum):
+class Suit(IntEnum):
+    CLUB = 1
+    DIAMOND = 2
+    HEART = 3
+    SPADE = 4
+
+class Rank(IntEnum):
     TWO = 2
     THREE = 3
     FOUR = 4
@@ -26,9 +50,3 @@ class Rank(Enum):
     QUEEN = 12
     KING = 13
     ACE = 14
-
-class Suit(Enum):
-    CLUB = 1
-    DIAMOND = 2
-    HEART = 3
-    SPADE = 4
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,3 +1,4 @@
+import pytest
 from cardbot.card import (
     Card,
     Rank,
@@ -7,3 +8,22 @@ from cardbot.card import (
 def test_is_easy():
     c = Card(rank=Rank.ACE, suit=Suit.CLUB)
     print(c)
+
+def test_suits_trump_rank():
+    ace = Card(rank=Rank.ACE, suit=Suit.CLUB)
+    king = Card(rank=Rank.KING, suit=Suit.SPADE)
+    assert ace < king
+
+def test_rank_for_same_suit():
+    ace = Card(rank=Rank.ACE, suit=Suit.CLUB)
+    king = Card(rank=Rank.KING, suit=Suit.CLUB)
+    assert ace > king
+
+def test_same_card():
+    ace = Card(rank=Rank.ACE, suit=Suit.CLUB)
+    ace2 = Card(rank=Rank.ACE, suit=Suit.CLUB)
+    assert ace == ace2
+
+def test_mixed_up_rank_and_suit(): #should pass because it errors
+    with pytest.raises(ValueError): #can declare exactly what error you want
+        Card(suit=Rank.ACE, rank=Suit.CLUB)
